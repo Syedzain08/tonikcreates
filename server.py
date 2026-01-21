0f18a1d138fb07f747b639750d2f10866bec29f6
Zain Ul Abideen <goodtingbye2@gmail.com>
2026-01-22 00:21:04 +0500

    removed admin  route without trailing '/'

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, redirect, url_for
 from flask_frozen import Freezer
 from shutil import copy
 import yaml
